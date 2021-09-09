c3cff57d07ecdedb1167d6eb3bf4c59b5a702a04
Leonardo Roese <leo.roese@hotmail.com>
2021-09-09 14:53:40 -0600

    feat: part3 start

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -127,7 +127,7 @@ export default NextAuth({
   // pages is not specified for that route.
   // https://next-auth.js.org/configuration/pages
   pages: {
-    // signIn: '/auth/signin',  // Displays signin buttons
+    signIn: '/signin', // Displays signin buttons
     // signOut: '/auth/signout', // Displays form with sign out button
     // error: '/auth/error', // Error code passed in query string as ?error=
     // verifyRequest: '/auth/verify-request', // Used for check email page
---
+++ src/pages/signin.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const signin: FC = () => (
+  <div>
+    <h1>Sup</h1>
+  </div>
+);
+
+export default signin;
